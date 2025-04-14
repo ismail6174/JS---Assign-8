d7ec568b13a42e620456078952bafd8bfd163bb4
Ismail Shah <ismailpx.6174@gmail.com>
2025-04-14 15:41:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function counting(num1,num2) {
+   var output = "";
+    for(var i = a; i <= b; i++){
+        output += i + " ";
+    }
+    return output;
+}
+ var a = +prompt("Enter first num");
+ var b =  +prompt("Enter last num");
+document.write(counting(a, b));
